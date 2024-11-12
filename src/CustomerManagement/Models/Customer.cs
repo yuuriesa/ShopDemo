03c604f280bb9fb55ab103c788b7cd384956a882
Yuri <yuri.salgado.ft@gmail.com>
2024-11-11 22:18:51 -0300

    Add validations to the Customer Model

--- src/CustomerManagement/Models/Customer.cs
+++ src/CustomerManagement/Models/Customer.cs
@@ -6,8 +6,14 @@ namespace CustomerManagement.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Name is Required", AllowEmptyStrings = false)]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "LastName is Required", AllowEmptyStrings = false)]
         public string? LastName { get; set; }
+        [EmailAddress(ErrorMessage = "Please provide a valid email...")]
+        [Required(ErrorMessage = "Email is Required")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Date of Birth is Required")]
         public DateTime DateOfBirth { get; set; }
     }
 }
