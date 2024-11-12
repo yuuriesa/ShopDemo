5b855c89cf37c6866cb09e730ab2cc88e059da22
Yuri <yuri.salgado.ft@gmail.com>
2024-11-12 11:01:27 -0300

    fixes model customer

--- src/CustomerManagement/Models/Customer.cs
+++ src/CustomerManagement/Models/Customer.cs
@@ -9,16 +9,16 @@ namespace CustomerManagement.Models
         [Required(ErrorMessage = "Name is Required", AllowEmptyStrings = false)]
         [MinLength(2, ErrorMessage = "Must Be at least 2 characters long")]
         [MaxLength(40, ErrorMessage = "Must have a maximum of 200 characters")]
-        [RegularExpression(@"^[a-zA-Z]{1,40}$", ErrorMessage = "Characters are not allowed.")]
-        public string? Name { get; set; }
+        [RegularExpression(@"^[a-zA-Z]{2,40}$", ErrorMessage = "Characters are not allowed.")]
+        public string Name { get; set; }
         [Required(ErrorMessage = "LastName is Required", AllowEmptyStrings = false)]
         [MinLength(2, ErrorMessage = "Must Be at least 2 characters long")]
-        [MaxLength(200, ErrorMessage = "Must have a maximum of 200 characters")]
-        [RegularExpression(@"^[a-zA-Z]{1,40}$", ErrorMessage = "Characters are not allowed.")]
-        public string? LastName { get; set; }
+        [MaxLength(40, ErrorMessage = "Must have a maximum of 200 characters")]
+        [RegularExpression(@"^[a-zA-Z]{2,40}$", ErrorMessage = "Characters are not allowed.")]
+        public string LastName { get; set; }
         [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
         [Required(ErrorMessage = "Email is Required")]
-        public string? Email { get; set; }
+        public string Email { get; set; }
         [Required(ErrorMessage = "Date of Birth is Required")]
         public DateTime DateOfBirth { get; set; }
     }
