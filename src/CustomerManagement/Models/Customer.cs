a6a783b0148d83ad2515a064c100eab2a3e68131
Yuri <yuri.salgado.ft@gmail.com>
2024-11-12 22:53:21 -0300

    fixes model customer and customerDto

--- src/CustomerManagement/Models/Customer.cs
+++ src/CustomerManagement/Models/Customer.cs
@@ -6,16 +6,11 @@ namespace CustomerManagement.Models
     {
         [Key]
         public int CustomerId { get; set; }
-        [Required(ErrorMessage = "Name is Required", AllowEmptyStrings = false)]
-        [MinLength(2, ErrorMessage = "Must Be at least 2 characters long")]
+        [Required(ErrorMessage = "FirstName is Required", AllowEmptyStrings = false)]
         [MaxLength(40, ErrorMessage = "Must have a maximum of 40 characters")]
-        [RegularExpression(@"^[a-zA-Z]{2,40}$")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "LastName is Required", AllowEmptyStrings = false)]
-        [MinLength(2, ErrorMessage = "Must Be at least 2 characters long")]
         [MaxLength(40, ErrorMessage = "Must have a maximum of 40 characters")]
-        [RegularExpression(@"^[a-zA-Z]{2,40}$")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
         [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
         [Required(ErrorMessage = "Email is Required")]
         public string Email { get; set; }
