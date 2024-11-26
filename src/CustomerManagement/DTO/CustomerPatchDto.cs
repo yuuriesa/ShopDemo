15276216d5a512e851ec01a613c38d257c717bfe
yurisalgado21 <yuri.salgado.ft@gmail.com>
2024-11-26 14:03:40 -0300

    add property Addresses in CustomerPatchDto

--- src/CustomerManagement/DTO/CustomerPatchDto.cs
+++ src/CustomerManagement/DTO/CustomerPatchDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerManagement.DTO;
 
 namespace CustomerManagement.Models
 {
@@ -18,5 +19,6 @@ namespace CustomerManagement.Models
         public string? Email { get; set; }
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+        public ICollection<AddressDto> Addresses { get; set; }
     }
 }
