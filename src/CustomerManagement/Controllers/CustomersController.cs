169ae8769564e6f7093ac55a529ec80b509ed734
yurisalgado21 <yuri.salgado.ft@gmail.com>
2024-12-08 10:03:39 -0300

    fixes

--- src/CustomerManagement/Controllers/CustomersController.cs
+++ src/CustomerManagement/Controllers/CustomersController.cs
@@ -30,7 +30,7 @@ namespace CustomerManagement.Controllers
         [HttpGet]
         public IActionResult GetAll(int pageNumber = 1, int pageSize = 10)
         {
-            if (pageNumber < 0 || pageSize < 0) return BadRequest();
+            if (pageNumber < 0 || pageSize < 0) return BadRequest("The pagination parameters 'pageSize' and 'pageNumber' must be positive numbers. Check the values ​​provided.");
 
             var validFilter = new PaginationFilter(pageNumber: pageNumber, pageSize: pageSize);
 
