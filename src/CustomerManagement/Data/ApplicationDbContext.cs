51002ae26553663f68ff195f0fed6788f2f0af7c
yurisalgado21 <yuri.salgado.ft@gmail.com>
2024-12-25 19:21:41 -0300

    DbSet<Order and Item> Added

--- src/CustomerManagement/Data/ApplicationDbContext.cs
+++ src/CustomerManagement/Data/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ namespace CustomerManagement.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
